5d6d05210cef3c8df543bbc858861162093aea35
imannaswini <iammannaswini@gmail.com>
2026-02-14 12:11:44 +0530

    Added modified  frontend folder

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
+import useAuthStore from "./context/AuthStore";
+
+useAuthStore.getState().initializeAuth(); // ✅ VERY IMPORTANT
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
